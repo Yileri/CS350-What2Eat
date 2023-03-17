a62d310cb99104f0b9ed0ddd3c3271539d34ac0a
Yileri <yigit.ileri@ozu.edu.tr>
2023-03-17 14:46:14 +0300

    getting input

--- What2Eat.c
+++ What2Eat.c
@@ -2,7 +2,14 @@
 
 int main() {
     int people;
+    char* choices[] = {"Kebab", "Hamburger", "Salad", "Lahmacun", "Pizza"}; 
+
     printf("Please enter the number of people: ");
-    scanf("%d", people);
+    scanf("%d", &people);
+
+    for (int i = 0; i < people; i++) {
+        printf("Person %d, please type the name of items to be the most wanted one in the beginning and least wanted at the end", i+1);
+
+    }
     return 0;
 }
