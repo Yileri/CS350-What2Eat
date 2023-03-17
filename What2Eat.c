6e5b69091de85ef3ef442e479fa7c1069603dcd6
Yileri <yigit.ileri@ozu.edu.tr>
2023-03-17 15:22:56 +0300

    getting inputs for choices

--- What2Eat.c
+++ What2Eat.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int people;
-    char* choices[] = {"Kebab", "Hamburger", "Salad", "Lahmacun", "Pizza"}; 
+    int* choices[] = {0, 0, 0, 0, 0}; 
 
     printf("Please enter the number of people: ");
     scanf("%d", &people);
 
     for (int i = 0; i < people; i++) {
-        printf("Person %d, please type the name of items to be the most wanted one in the beginning and least wanted at the end", i+1);
+        printf("\n");
+        printf("Person %d, please type the name of items to be the most wanted one in the beginning and least wanted at the end\n", i+1);
+        printf("1. Kebab\n");
+        printf("2. Hamburger\n");
+        printf("3. Salad\n");
+        printf("4. Lahmacun\n");
+        printf("5. Pizza\n");
+        printf("\n");
 
+        for (int j = 0; j < 5; j++) {
+            char item[20];
+            printf("Choice number %d: ", j+1);
+            scanf("%s", item);
+
+            if (!strcmp(item, "Kebab")) {
+                choices[0] += 5-j;
+            } else if (!strcmp(item, "Hamburger")) {
+                choices[1] += 5-j;
+            } else if (!strcmp(item, "Salad")) {
+                choices[2] += 5-j;
+            } else if (!strcmp(item, "Lahmacun")) {
+                choices[3] += 5-j;
+            } else if (!strcmp(item, "Pizza")) {
+                choices[4] += 5-j;
+            } else {
+                printf("\n");
+                printf("Invalid item, please type an item from the list\n");
+                printf("\n");
+                j--;
+            }
+        }
     }
     return 0;
 }
